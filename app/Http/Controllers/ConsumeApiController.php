831d8225522d122359220ae83a23daf48afd6e01
aligilani5 <airtaza753@gmail.com>
2022-10-26 15:57:38 +0500

    ..

--- app/Http/Controllers/ConsumeApiController.php
+++ app/Http/Controllers/ConsumeApiController.php
@@ -12,19 +12,18 @@ class ConsumeApiController extends Controller
     public function getCategoriesWithProductsPrice(){
 
         $response = Http::get('http://localhost:8000/api/getAllCategoriesWithProductsPrice')
-        ->collect();
-        // dd($response);
-        // return $response;
-         return view('categories_products_price', ['response'=>$response]);
+        ->json();
+        
+        return view('categories_products_price', ['response'=>$response]);
     }
 
 
     //getCategoriesWithNewListedProducts
     public function getCategoriesWithNewListedProducts(){
         $response = Http::get('http://localhost:8000/api/getCategoriesWithNewListedProducts')
-        ->collect();
+        ->json();
 
-        return $response;
+        return view('top_categories_with_products', ['response'=>$response]);
     }
 
     //getAllCategoriesWithAtleastProduct
